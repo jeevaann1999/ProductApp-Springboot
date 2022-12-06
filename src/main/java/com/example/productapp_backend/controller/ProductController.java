4afb360f9d505096b94f4199ce76833811dd4ff4
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-06 05:55:26 +0530

    homepage

--- src/main/java/com/example/productapp_backend/controller/ProductController.java
+++ src/main/java/com/example/productapp_backend/controller/ProductController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController
 {
-
  @PostMapping("/")
+ public  String HomePage(){
+  return "Wlcome to Product App Homepage";
+ }
+ @PostMapping("/productadd")
  public String ProductAdd(){
   return "Welcome to my Product Add page";
  }
- @PostMapping("/search")
+ @PostMapping("/productsearch")
  public String ProductSearch(){
   return "Welcome to my Product Search page";
  }
